1917c3cdc7c81f635a1b394e46636c61ec11df71
Lucas Corsi <corsi.lucasgabriel.studente@itispaleocapa.it>
2023-03-21 20:45:15 +0100

    funzione cancellazione e modifca

--- Corsi - Strutture file/Form1.Designer.cs
+++ Corsi - Strutture file/Form1.Designer.cs
@@ -35,27 +35,35 @@
             this.Svuotamento = new System.Windows.Forms.Button();
             this.listView1 = new System.Windows.Forms.ListView();
             this.cancellazione = new System.Windows.Forms.Button();
+            this.modifica = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(47, 129);
+            this.textBox1.Location = new System.Drawing.Point(63, 122);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.Size = new System.Drawing.Size(132, 22);
             this.textBox1.TabIndex = 0;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(226, 129);
+            this.textBox2.Location = new System.Drawing.Point(301, 122);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.Size = new System.Drawing.Size(132, 22);
             this.textBox2.TabIndex = 1;
             // 
             // Salva
             // 
-            this.Salva.Location = new System.Drawing.Point(47, 199);
+            this.Salva.Location = new System.Drawing.Point(63, 245);
+            this.Salva.Margin = new System.Windows.Forms.Padding(4);
             this.Salva.Name = "Salva";
-            this.Salva.Size = new System.Drawing.Size(75, 23);
+            this.Salva.Size = new System.Drawing.Size(100, 28);
             this.Salva.TabIndex = 2;
             this.Salva.Text = "Salva";
             this.Salva.UseVisualStyleBackColor = true;
@@ -63,9 +71,10 @@
             // 
             // salvataggio
             // 
-            this.salvataggio.Location = new System.Drawing.Point(149, 199);
+            this.salvataggio.Location = new System.Drawing.Point(199, 245);
+            this.salvataggio.Margin = new System.Windows.Forms.Padding(4);
             this.salvataggio.Name = "salvataggio";
-            this.salvataggio.Size = new System.Drawing.Size(75, 23);
+            this.salvataggio.Size = new System.Drawing.Size(100, 28);
             this.salvataggio.TabIndex = 3;
             this.salvataggio.Text = "Salvataggio";
             this.salvataggio.UseVisualStyleBackColor = true;
@@ -73,9 +82,10 @@
             // 
             // Svuotamento
             // 
-            this.Svuotamento.Location = new System.Drawing.Point(47, 382);
+            this.Svuotamento.Location = new System.Drawing.Point(63, 470);
+            this.Svuotamento.Margin = new System.Windows.Forms.Padding(4);
             this.Svuotamento.Name = "Svuotamento";
-            this.Svuotamento.Size = new System.Drawing.Size(75, 23);
+            this.Svuotamento.Size = new System.Drawing.Size(100, 28);
             this.Svuotamento.TabIndex = 4;
             this.Svuotamento.Text = "Svuota file";
             this.Svuotamento.UseVisualStyleBackColor = true;
@@ -84,27 +94,81 @@
             // listView1
             // 
             this.listView1.HideSelection = false;
-            this.listView1.Location = new System.Drawing.Point(491, 51);
+            this.listView1.Location = new System.Drawing.Point(655, 63);
+            this.listView1.Margin = new System.Windows.Forms.Padding(4);
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(224, 354);
+            this.listView1.Size = new System.Drawing.Size(297, 435);
             this.listView1.TabIndex = 5;
             this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.List;
             // 
             // cancellazione
             // 
-            this.cancellazione.Location = new System.Drawing.Point(47, 249);
+            this.cancellazione.Location = new System.Drawing.Point(63, 306);
+            this.cancellazione.Margin = new System.Windows.Forms.Padding(4);
             this.cancellazione.Name = "cancellazione";
-            this.cancellazione.Size = new System.Drawing.Size(85, 23);
+            this.cancellazione.Size = new System.Drawing.Size(113, 28);
             this.cancellazione.TabIndex = 6;
             this.cancellazione.Text = "Cancellazione";
             this.cancellazione.UseVisualStyleBackColor = true;
             this.cancellazione.Click += new System.EventHandler(this.cancellazione_Click);
             // 
+            // modifica
+            // 
+            this.modifica.Location = new System.Drawing.Point(199, 310);
+            this.modifica.Name = "modifica";
+            this.modifica.Size = new System.Drawing.Size(100, 23);
+            this.modifica.TabIndex = 7;
+            this.modifica.Text = "modifica";
+            this.modifica.UseVisualStyleBackColor = true;
+            this.modifica.Click += new System.EventHandler(this.modifica_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(63, 189);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(132, 22);
+            this.textBox3.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(63, 99);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 16);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Input";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(298, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 16);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Prezzo";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(63, 169);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(112, 16);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Parola modificata";
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.modifica);
             this.Controls.Add(this.cancellazione);
             this.Controls.Add(this.listView1);
             this.Controls.Add(this.Svuotamento);
@@ -112,6 +176,7 @@
             this.Controls.Add(this.Salva);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -128,6 +193,11 @@
         private System.Windows.Forms.Button Svuotamento;
         private System.Windows.Forms.ListView listView1;
         private System.Windows.Forms.Button cancellazione;
+        private System.Windows.Forms.Button modifica;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- Corsi - Strutture file/Form1.cs
+++ Corsi - Strutture file/Form1.cs
@@ -8,54 +8,120 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace Corsi___Strutture_file
 {
     public partial class Form1 : Form
     {
         string file, appoggio;
-        int indice;
         string line;
+        StreamReader reader = null;
+        StreamWriter writer = null;
+        StreamWriter writerApp = null;
         public Form1()
         {
             InitializeComponent();
             file = @"File.csv";
             appoggio = @"FileAppoggio.csv";
-            indice = 0;
         }
 
         private void Salva_Click(object sender, EventArgs e)
         {
-
-            StreamWriter writer = new StreamWriter(file, true);
+            writer = new StreamWriter(file, true);
             writer.WriteLine("nome: " + textBox1.Text + " ; " + "prezzo: " + textBox2.Text);
             writer.Close();
-
         }
 
         private void Svuotamento_Click(object sender, EventArgs e)
         {
-            StreamWriter f = new StreamWriter(file, false);
-            f.Close();
+            writer = new StreamWriter(file, false);
+            writer.Close();
         }
 
         private void salvataggio_Click(object sender, EventArgs e)
         {
-
-            StreamReader reader = new StreamReader(file);
+            reader = new StreamReader(file);
             line = reader.ReadLine();
             while (line != null)
             {
+                listView1.Clear();
                 listView1.Items.Add(line);
                 line = reader.ReadLine();
             }
             reader.Close();
-
         }
 
         private void cancellazione_Click(object sender, EventArgs e)
         {
+            writerApp = new StreamWriter(appoggio, false);
+            Cancella();
+            writerApp.Close();
+            Sosituzione()
         }
 
+        private void modifica_Click(object sender, EventArgs e)
+        {
+            writerApp = new StreamWriter(appoggio, false);
+            Modifica();
+            writerApp.Close();
+            Sosituzione();
+        }
+
+        public void Cancella()
+        {
+            reader = new StreamReader(file);
+            line = reader.ReadLine();
+            
+            while (line != null)
+            {
+                String[] splitter = line.Split(';');
+                String[] splitter2 = splitter[0].Split(' ');
+                if (splitter2[1] != textBox1.Text)
+                {
+                    writerApp.WriteLine(line);
+                }
+
+                line = reader.ReadLine();
+                
+            }
+            reader.Close();
+        }
+
+        public void Modifica()
+        {
+            reader = new StreamReader(file);
+            line = reader.ReadLine();
+            while (line != null)
+            {
+                String[] splitter = line.Split(';');
+                String[] splitter2 = splitter[0].Split(' ');
+                if (splitter2[1] != textBox1.Text)
+                {
+                    writerApp.WriteLine(line);
+                }
+                else if(splitter2[1] == textBox1.Text)
+                {
+                    writerApp.WriteLine("nome: " + textBox3.Text + " ; " + "prezzo: " + textBox2.Text);
+                }
+
+                line = reader.ReadLine();
+                
+            }
+            reader.Close();
+        }
+
+        public void Sosituzione()
+        {
+            File.Delete(file);
+            File.Move(appoggio, file);
+            listView1.Clear();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
+    
 }
